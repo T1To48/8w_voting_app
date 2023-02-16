116097ec559738603757314a3df269e2f310d6f3
T1To48 <tofek.98@gmail.com>
2023-02-16 20:04:40 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
 function App() {
+  const x=()=>{
+    for(let y=0;y<=10;y++){
+      <div>
+      hello world {y}
+    </div>
+    }
+  }
   return (
     <div>
-      hello world
+      {x()}
     </div>
   );
 }
