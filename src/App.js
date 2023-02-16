3407784543c3bc17fa4dfdce2aab3fc8559399e1
T1To48 <tofek.98@gmail.com>
2023-02-16 20:11:43 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 
 function App() {
-  const x=()=>{
-    for(let y=0;y<=10;y++){
-      <div>
-      hello world {y}
-    </div>
-    }
-  }
   return (
     <div>
-      {x()}
+      hey there
     </div>
   );
 }
